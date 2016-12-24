0db2c5c856b4cf0e1d47bf9213bc24d9fed02fe1
chinmay <chinmay.nagarkar@gmail.com>
2016-12-24 01:42:40 -0800

    Shorter URL for google reviews

--- apps/ionic/medval/src/pages/survey/RequestReviewComponnet/requestreview.component.ts
+++ apps/ionic/medval/src/pages/survey/RequestReviewComponnet/requestreview.component.ts
@@ -13,7 +13,7 @@ import {InAppBrowser} from "ionic-native";
 @RegisterComponent
 export class RequestReviewComponent {
 
-  static GOOGLE_REVIEW_URL: string = `https://www.google.com/search?q=orthodontic%20excellence%20%20%20%20%20%20&%3A-1%2Clf_msr%3A-1%2Clf%3A1%2Clf_ui%3A2%2Clf_pqs%3AEAE%20%20%20%20%20%20&rflfq=1%20%20%20%20%20%20&rlha=0%20%20%20%20%20%20&rllag=47364907%2C-122235939%2C23993%20%20%20%20%20%20&tbm=lcl%20%20%20%20%20%20&rldimm=13072678170866571203%20%20%20%20%20%20&lrd=0x5490691ef2188207%3A0xb56b7b88cd8157c3%2C1%2C%20%20%20%20%20%20&rct=j#lrd=0x5490691ef2188207:0xb56b7b88cd8157c3,3,&rlfi=hd:;si:13072678170866571203;mv:!1m3!1d30071.983337186946!2d-122.15809474999999!3d47.54806575!2m3!1f0!2f0!3f0!3m2!1i62!2i268!4f13.1`;
+  static GOOGLE_REVIEW_URL: string = `https://www.google.com/search?q=orthodontic+excellence+newcastle&oq=orthodontic+excellence+newcastle&lrd=0x5490691ef2188207:0xb56b7b88cd8157c3,1,#lrd=0x5490691ef2188207:0xb56b7b88cd8157c3,1,`;
   static FACEBOOK_REVIEW_URL: string = `https://www.facebook.com/pg/NewcastleOE/reviews/?ref=page_internal#u_0_r`;
   static BROWSER_OPTIONS: string =     'location=no,toolbar=yes,hardwareback=no,clearcache=yes,clearsessioncache=yes,closebuttoncaption=Back';
 
