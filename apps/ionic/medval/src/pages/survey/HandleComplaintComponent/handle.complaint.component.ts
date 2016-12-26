ee386f95bc8318f7e508e527a169d0fbe238ab09
SantoshSah <santoshsah4@gmail.com>
2016-12-26 11:39:20 +0545

    modified files of handleComplaintComponent and requestReviewComponent

--- apps/ionic/medval/src/pages/survey/HandleComplaintComponent/handle.complaint.component.ts
+++ apps/ionic/medval/src/pages/survey/HandleComplaintComponent/handle.complaint.component.ts
@@ -1,13 +1,17 @@
-import {Component} from "@angular/core";
-import {NavController} from "ionic-angular";
+import {Component, AfterViewChecked, AfterViewInit} from '@angular/core';
+
+import { NavController} from 'ionic-angular';
+import {Config} from "../../../shared/aws/config";
+import {StartComponent} from "../start/start.component";
 import {Utils} from "../../../shared/stuff/utils";
 import {SessionService} from "../../../services/session/delegator";
+import {MedvalComponent} from "../../../shared/stuff/medval.component";
 import {AccessTokenService} from "../../../shared/aws/access.token.service";
 import {SurveyNavUtils} from "../SurveyNavUtils";
 import {RegisterComponent} from "../../../services/survey/survey.navigator";
-import {AccountService} from "../../../services/account/delegator";
-import {Config} from "../../../shared/aws/config";
-import {Account} from "../../../services/account/schema";
+
+import {Validators, FormBuilder } from '@angular/forms';
+import { ValidationService } from '../../../shared/components/validator/validation.service';
 
 @Component({
   templateUrl: 'handle.complaint.component.html',
@@ -15,25 +19,23 @@ import {Account} from "../../../services/account/schema";
 
 @RegisterComponent
 export class HandleComplaintComponent {
-
-  private images: string[] = [
-    "http://www.nicephotomag.com/wp-content/uploads/2009/06/uniracer_2009_0409_1435_20_149.jpg",
-    "https://s-media-cache-ak0.pinimg.com/236x/eb/ac/c2/ebacc2789ff96ca7aafe6c855b6a8e1a.jpg",
-    "http://i.dailymail.co.uk/i/pix/2011/04/14/article-1376796-0B9F664100000578-491_634x467.jpg",
-    "http://cdn.alex.leonard.ie/wp-content/uploads/2013/02/extreme-mountain-unicycling.jpg"
-  ];
-  complaintMsg: string;
-  image: string = this.images[0];
-  account: Account = new Account();
+  emailProviders: string[] = [];
+  emailProvidersOrg:string[]= ['@gmail.com','@yahoo.com','@mail.com','@outlook.com'];
+  public complaintMsg: string;
+  public user:any;
 
   constructor(
     tokenProvider: AccessTokenService,
-    accountSvc: AccountService,
     private sessionSvc: SessionService,
     private navCtrl: NavController,
-    private utils: Utils
+    private utils: Utils,
+    private formBuilder: FormBuilder
   ) {
-    this.account =  accountSvc.getCached(Config.CUSTOMERID);
+     this.user = this.formBuilder.group({
+      email: ['', ],
+      tel: ['', Validators.compose([ValidationService.phoneValidator])],
+      complaintMsg: ['', ]
+    });
   }
 
   public navigateToNext() {
@@ -42,7 +44,7 @@ export class HandleComplaintComponent {
       this.navCtrl,
       this.utils,
       "We're sorry if had a bad experience",
-      "'The folks at '+ account.properties.customerName + ' want to do better'",
+      "The folks at Orthodontic Excellence want to do better",
       "Rest assured that your feedback, although nameless, will help");
   }
 }
---
+++ apps/ionic/medval/src/shared/components/validator/validator.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ValidationService } from '../../../shared/components/validator/validation.service';
+
+@Component({
+  selector: 'validator',
+  template: `<div *ngIf="errorMessage !== null" style="color:#E82C0C;">{{errorMessage}}</div>`
+})
+export class ValidatorComponent {
+  @Input() control: FormControl;
+  constructor() { }
+
+  get errorMessage() {
+    for (let propertyName in this.control.errors) {
+      if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
+        return ValidationService.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
+      }
+    }
+    return null;
+  }
+}
