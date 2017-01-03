e5cec5cfeac66fcf423713fe2475dbf08c108b23
SantoshSah <santoshsah4@gmail.com>
2017-01-03 12:18:00 +0545

    implemented ng2-auto-complete

--- apps/ionic/medval/src/app/app.module.ts
+++ apps/ionic/medval/src/app/app.module.ts
@@ -1,60 +1,61 @@
 //import { RouterModule }   from '@angular/router';
-import {NgModule, enableProdMode} from "@angular/core";
-import {IonicApp, IonicModule} from "ionic-angular";
-import {MyApp} from "./app.component";
-import {LoginComponent} from "../pages/login/login.component";
-import {Utils} from "../shared/stuff/utils";
-import {Config} from "../shared/aws/config";
-import {AccessTokenService} from "../shared/aws/access.token.service";
-import {AccountComponent} from "../pages/account/account.component";
-import {DashboardComponent} from "../pages/dashboard/dashboard.component";
-import {StaffComponent} from "../pages/staff/staff.component";
-import {TermComponent} from "../pages/dashboard/terms/term.component";
-import {PolicyComponent} from "../pages/dashboard/policy/policy.component";
-import {HttpModule, JsonpModule} from "@angular/http";
-import {RatingComponent} from "../shared/rating/rating.component";
-import {StartComponent} from "../pages/survey/start/start.component";
-import {ThanksComponent} from "../pages/survey/thanks/thanks.component";
-import {StaffEditComponent} from "../pages/staff/staff.edit.component";
-import {CarouselComponent} from "../pages/survey/carousel/carousel.component";
-import {PickStaffComponent} from "../pages/survey/pickstaff/pickstaff.component";
-import {LiveStaffService} from "../services/staff/live";
-import {MockStaffService} from "../services/staff/mock";
-import {StaffService} from "../services/staff/delegator";
-import {LiveAccountService} from "../services/account/live";
-import {MockAccountService} from "../services/account/mock";
-import {AccountService} from "../services/account/delegator";
-import {LiveSessionService} from "../services/session/live";
-import {MockSessionService} from "../services/session/mock";
-import {SessionService} from "../services/session/delegator";
-import {FeedbackComponent} from "../shared/feedback/feedback.component";
-import {HeaderComponent} from "../shared/header/header.component";
-import {MetricService} from "../services/metric/delegator";
-import {MockMetricService} from "../services/metric/mock";
-import {LiveMetricService} from "../services/metric/live";
-import {NpsTrendComponent} from "../pages/charts/nps.trend.component";
-import {RevvolveMetricsComponent} from "../pages/charts/revvolve.metrics.component";
-import {AllTrendsComponent} from "../pages/charts/all.trends";
-import {SettingsComponent} from "../pages/settings/settings.component";
-import {MetricSummaryComponent} from "../pages/metricsetup/metric.summary.component";
-import {MetricDetailComponent} from "../pages/metricsetup/metric.detail.component";
-import {ServiceFactory} from "../services/service.factory";
-import {SurveySelectionComponent} from "../pages/survey/surveyselection/surveyselection.component";
-import {SurveyService} from "../services/survey/delegator";
-import {MockSurveyService} from "../services/survey/mock";
-import {LiveSurveyService} from "../services/survey/live";
-import {StartWithSurveyOption} from "../pages/survey/startWithSurveyOption/start.with.survey.option.component";
-import {TopInfluencerComponent} from "../pages/survey/TopInfluencerComponent/top.influencer.component";
-import {HandleComplaintComponent} from "../pages/survey/HandleComplaintComponent/handle.complaint.component";
-import {SingleMetricComponent} from "../pages/survey/singlemetric/single.metric.component";
-import {ToplineForStaffComponent} from "../pages/survey/topline.for.staff/topline.for.staff.component";
-import {MultimetricComponent} from "../pages/survey/multimetric/multimetric.component";
-import {RequestReviewComponent2} from "../pages/survey/RequestReviewComponent2/requestreview.component2";
-import {CustomerTextEmailComponent} from "../pages/survey/RequestReviewComponent2/customer.text.email.component";
-import {SReplacer} from "../pipes/SReplacer";
-import {RotatingCarousel} from "../pages/survey/rotatingcarousel/rotatingcarousel.component";
-import {WheelComponent} from "../pages/wheel/wheel.component";
+import { NgModule, enableProdMode } from "@angular/core";
+import { IonicApp, IonicModule } from "ionic-angular";
+import { MyApp } from "./app.component";
+import { LoginComponent } from "../pages/login/login.component";
+import { Utils } from "../shared/stuff/utils";
+import { Config } from "../shared/aws/config";
+import { AccessTokenService } from "../shared/aws/access.token.service";
+import { AccountComponent } from "../pages/account/account.component";
+import { DashboardComponent } from "../pages/dashboard/dashboard.component";
+import { StaffComponent } from "../pages/staff/staff.component";
+import { TermComponent } from "../pages/dashboard/terms/term.component";
+import { PolicyComponent } from "../pages/dashboard/policy/policy.component";
+import { HttpModule, JsonpModule } from "@angular/http";
+import { RatingComponent } from "../shared/rating/rating.component";
+import { StartComponent } from "../pages/survey/start/start.component";
+import { ThanksComponent } from "../pages/survey/thanks/thanks.component";
+import { StaffEditComponent } from "../pages/staff/staff.edit.component";
+import { CarouselComponent } from "../pages/survey/carousel/carousel.component";
+import { PickStaffComponent } from "../pages/survey/pickstaff/pickstaff.component";
+import { LiveStaffService } from "../services/staff/live";
+import { MockStaffService } from "../services/staff/mock";
+import { StaffService } from "../services/staff/delegator";
+import { LiveAccountService } from "../services/account/live";
+import { MockAccountService } from "../services/account/mock";
+import { AccountService } from "../services/account/delegator";
+import { LiveSessionService } from "../services/session/live";
+import { MockSessionService } from "../services/session/mock";
+import { SessionService } from "../services/session/delegator";
+import { FeedbackComponent } from "../shared/feedback/feedback.component";
+import { HeaderComponent } from "../shared/header/header.component";
+import { MetricService } from "../services/metric/delegator";
+import { MockMetricService } from "../services/metric/mock";
+import { LiveMetricService } from "../services/metric/live";
+import { NpsTrendComponent } from "../pages/charts/nps.trend.component";
+import { RevvolveMetricsComponent } from "../pages/charts/revvolve.metrics.component";
+import { AllTrendsComponent } from "../pages/charts/all.trends";
+import { SettingsComponent } from "../pages/settings/settings.component";
+import { MetricSummaryComponent } from "../pages/metricsetup/metric.summary.component";
+import { MetricDetailComponent } from "../pages/metricsetup/metric.detail.component";
+import { ServiceFactory } from "../services/service.factory";
+import { SurveySelectionComponent } from "../pages/survey/surveyselection/surveyselection.component";
+import { SurveyService } from "../services/survey/delegator";
+import { MockSurveyService } from "../services/survey/mock";
+import { LiveSurveyService } from "../services/survey/live";
+import { StartWithSurveyOption } from "../pages/survey/startWithSurveyOption/start.with.survey.option.component";
+import { TopInfluencerComponent } from "../pages/survey/TopInfluencerComponent/top.influencer.component";
+import { HandleComplaintComponent } from "../pages/survey/HandleComplaintComponent/handle.complaint.component";
+import { SingleMetricComponent } from "../pages/survey/singlemetric/single.metric.component";
+import { ToplineForStaffComponent } from "../pages/survey/topline.for.staff/topline.for.staff.component";
+import { MultimetricComponent } from "../pages/survey/multimetric/multimetric.component";
+import { RequestReviewComponent2 } from "../pages/survey/RequestReviewComponent2/requestreview.component2";
+import { CustomerTextEmailComponent } from "../pages/survey/RequestReviewComponent2/customer.text.email.component";
+import { SReplacer } from "../pipes/SReplacer";
+import { RotatingCarousel } from "../pages/survey/rotatingcarousel/rotatingcarousel.component";
+import { WheelComponent } from "../pages/wheel/wheel.component";
 import { ValidatorComponent } from '../shared/components/validator/validator.component';
+import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
 
 enableProdMode();
 
@@ -114,7 +115,8 @@ enableProdMode();
   imports: [
     IonicModule.forRoot(MyApp),
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    Ng2AutoCompleteModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -164,9 +166,9 @@ enableProdMode();
 
   ],
   providers: [
-    { provide: Config, useClass: Config},
+    { provide: Config, useClass: Config },
     { provide: Utils, useClass: Utils },
-    { provide: AccessTokenService, useClass: AccessTokenService},
+    { provide: AccessTokenService, useClass: AccessTokenService },
     StaffService, MockStaffService, LiveStaffService,
     AccountService, MockAccountService, LiveAccountService,
     LiveSessionService, MockSessionService, SessionService,
@@ -176,7 +178,7 @@ enableProdMode();
   ]
 })
 
-export class AppModule {}
+export class AppModule { }
 
 export function final(target: any, propertyKey: string) {
   const value: any = target[propertyKey];
