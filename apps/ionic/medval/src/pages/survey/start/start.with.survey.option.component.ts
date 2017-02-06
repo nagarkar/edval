432397768de6654c2dfa9cdea674e33c589376a6
chinmay <chinmay.nagarkar@gmail.com>
2017-02-05 21:33:48 -0800

    Better fix to refresh survey start page.

--- apps/ionic/medval/src/pages/survey/start/start.with.survey.option.component.ts
+++ apps/ionic/medval/src/pages/survey/start/start.with.survey.option.component.ts
@@ -18,7 +18,6 @@ import {ErrorType} from "../../../shared/stuff/error.types";
 
 export class StartWithSurveyOption {
 
-  private static REFRESH_TIMER: number = 0;
   private images = [
     'http://img.picturequotes.com/2/26/25637/when-you-know-better-you-do-better-quote-1.jpg',
     'http://img.picturequotes.com/2/27/26724/its-not-intentions-that-matter-its-actions-we-are-what-we-do-and-say-not-what-we-intend-to-quote-1.jpg',
@@ -51,53 +50,35 @@ export class StartWithSurveyOption {
     navParams: NavParams
   ) {
 
-    StartWithSurveyOption.clearRefreshTimer();
     this.defaultOnly = navParams.get("defaultOnly") === true || this.defaultOnly;
     this.cancelPreviousSession = navParams.get("cancelPreviousSession") || this.cancelPreviousSession;
-
-    new ObjectCycler<string>(null, ...this.images)
-      .onNewObj.subscribe((next:string)=>this.leftImage = next);
-
   }
 
   ngOnInit() {
+    new ObjectCycler<string>(null, ...this.images)
+      .onNewObj.subscribe((next:string)=>this.leftImage = next);
     this.surveySvc.list().then((surveys: Survey[]) => {
       this.surveys = surveys;
       this.surveys.filter((survey: Survey)=> {
         return survey.id == 'default';
       })
     });
-    StartWithSurveyOption.startRefresh(()=>{
-      this.navCtrl.push(StartWithSurveyOption, {defaultOnly: this.defaultOnly});
-    });
-  }
-
-  static clearRefreshTimer() {
-    clearInterval(StartWithSurveyOption.REFRESH_TIMER);
-    StartWithSurveyOption.REFRESH_TIMER = 0;
-  }
-
-  static startRefresh(callback) {
-    if (StartWithSurveyOption.REFRESH_TIMER != 0) {
-      StartWithSurveyOption.clearRefreshTimer();
-    }
-    StartWithSurveyOption.REFRESH_TIMER = setTimeout(()=>{
-      callback();
-    }, 10 * 60 * 1000);
+    setTimeout(()=>{
+      if (this.navCtrl.getActive().component == StartWithSurveyOption) {
+        this.navCtrl.setRoot(StartWithSurveyOption, {defaultOnly: this.defaultOnly});
+      }
+    }, 1 * 60 * 1000)
   }
 
   gotoLogin() {
-    StartWithSurveyOption.clearRefreshTimer();
     this.navCtrl.setRoot(LoginComponent);
   }
 
   noThanks() {
-    StartWithSurveyOption.clearRefreshTimer();
     this.navCtrl.setRoot(ThanksComponent, {message: ["That's ok, maybe next time!"]});
   }
 
   pickSurvey(id: string){
-    StartWithSurveyOption.clearRefreshTimer();
     if (this.sessionSvc.hasCurrentSession() && !this.cancelPreviousSession) {
       try {
         this.sessionSvc.closeCurrentSession();
